4491e824ad485c9cdcf1fba201890f8d95ae899f
datntdev968 <datntdev968@gmail.com>
2025-08-14 21:45:59 +0700

    update code

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +29,38 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ValidationException) {
+            $error = $exception->errors();
+            $firstError = reset($error);
+
+            if ($request->ajax()) {
+                return response()->json([
+                    'message' => $firstError[0]
+                ], 422);
+            }
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'message' => 'Không tìm thấy dữ liệu!',
+            ], 404);
+        }
+
+        return parent::render($request, $exception);
+    }
+
+    public function report(Throwable $exception)
+    {
+        Log::error('>>Exception occurred<<', [
+            'message' => $exception->getMessage(),
+            'file'    => $exception->getFile(),
+            'line'    => $exception->getLine(),
+        ]);
+
+        parent::report($exception);
     }
 }
---
+++ resources/views/admin/layout/style.blade.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/demo.css.map') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/fonts.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/fonts.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/kaiadmin.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/kaiadmin.css.map') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/plugins.css.map') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/plugins.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/kaiadmin.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+
+<link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css">
+
+@stack('style')
